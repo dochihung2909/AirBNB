dd27401cafbcbab656ee0237d341563b667b821a
dochihung2909 <thestarss321@gmail.com>
2023-01-10 07:47:56 +0700

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+window.addEventListener('scroll', function () { 
+    const header = document.querySelector('.navbar__top');
+    header.classList.toggle("sticky", window.scrollY > 58);
+})
+
+// Nút play
+
+// Thêm nút Play nha chưa biết làm
+
+window.addEventListener('scroll', function () { 
+    const header = document.querySelector('.navbar__top');
+    header.classList.toggle("sticky", window.scrollY > 58);
+})
